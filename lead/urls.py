ee7f7058b08e8c0d25f4f386c1c62416c9526c8f
zouxiang <zouxiangjob@163.com>
2025-10-13 21:12:35 +0800

    Changes

--- lead/urls.py
+++ lead/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
